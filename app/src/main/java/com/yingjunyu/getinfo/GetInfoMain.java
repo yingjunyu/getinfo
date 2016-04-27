a0b05868d46d308d219a2bdfb7bf31d80d8290e7
yingjunyu <yingjunyu@163.com>
2016-04-27 13:20:06 +0800

    增加新闻页面（暂无实际内容）

--- app/src/main/java/com/yingjunyu/getinfo/GetInfoMain.java
+++ app/src/main/java/com/yingjunyu/getinfo/GetInfoMain.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.yingjunyu.getinfo.ui.ui_imp;
+import com.yingjunyu.getinfo.ui.ui_news;
 import android.content.Intent;
 import android.widget.SeekBar;
 import android.support.v7.widget.CardView;
@@ -145,7 +146,8 @@ public class GetInfoMain extends AppCompatActivity
             Intent intent = new Intent(this, ui_imp.class);
             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
-
+            Intent intent = new Intent(this, ui_news.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
